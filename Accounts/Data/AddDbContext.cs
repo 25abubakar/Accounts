267796bcd5ead1aca98448110d01253fc3fac6f2
LALTECH\abubakar_291 <muhammadabubakarkhan1234@gmail.com>
2026-05-04 15:49:05 +0500

    Add project files.

---
+++ Accounts/Data/AddDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Accounts.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
