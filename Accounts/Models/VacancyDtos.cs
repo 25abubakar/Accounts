b9dd3c3438238be1e4eafd3aa7ecb5f378548842
ubaidullah <ubaidullah@SW--1>
2026-05-13 11:20:00 +0500

    0

--- Accounts/Models/VacancyDtos.cs
+++ Accounts/Models/VacancyDtos.cs
@@ -29,7 +29,7 @@ namespace Accounts.Models
     {
         [Required]
         public int OrganizationId { get; set; }
-
+        public int vaccancycount { get; set; }
         /// <summary>
         /// e.g. "Manager", "Developer", "Head of Department"
         /// VacancyCode is auto-generated from this — do NOT send VacancyCode.
